dbf792e3fa19d1ba72a92bfae540d55b7939558a
katrina smith <ksmith10@ggc.edu>
2022-10-31 11:55:38 -0400

    Second commit

--- src/test/java/com/smithexperience/springjenkins/SpringJenkins2ApplicationTests.java
+++ src/test/java/com/smithexperience/springjenkins/SpringJenkins2ApplicationTests.java
@@ -16,6 +16,7 @@ class SpringJenkins2ApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test is executing... ");
+		logger.info("Test case executing second log statement... ");
 		assertEquals(true, true);
 	}
 
